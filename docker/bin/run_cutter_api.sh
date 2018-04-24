7d54d570d36a4cf66d3f37acdeff36c3b28b52eb
Kacper Pikulski <pikulak1@gmail.com>
2018-04-24 14:12:24 +0200

    Initialize Docker environment

---
+++ docker/bin/run_cutter_api.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# wait until postgres up
+/wait-for-it.sh postgres:5432
+
+cd cutter
+
+python manage.py makemigrations cutter_api
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
