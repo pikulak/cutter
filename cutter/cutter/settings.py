71dcacd0a1e8030d29562f46096e248efb0f1594
Kacper Pikulski <pikulak1@gmail.com>
2018-04-24 22:44:35 +0200

    Setup celery

--- cutter/cutter/settings.py
+++ cutter/cutter/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Celery
+
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672/'
+CELERY_TIMEZONE = 'UTC'
