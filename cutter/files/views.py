7f0ee4619ebe14d4f561b72c27c0d4a902560e67
Kacper Pikulski <pikulak1@gmail.com>
2018-04-25 09:58:51 +0200

    Add create() method to FileViewSet

--- cutter/files/views.py
+++ cutter/files/views.py
@@ -1,4 +1,6 @@
-from rest_framework.viewsets import(
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import (
     GenericViewSet,
     mixins
 )
@@ -14,3 +16,10 @@ class FileViewSet(mixins.CreateModelMixin,
     """
     queryset = FileAudio.objects.all()
     serializer_class = FileAudioSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
