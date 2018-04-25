1f6aeffd1d038a714d8a3ef5a177655e4c7d6f5a
Kacper Pikulski <pikulak1@gmail.com>
2018-04-25 07:09:12 +0200

    Add FileAudioSerializer

---
+++ cutter/files/serializers.py
@@ -0,0 +1,8 @@
+from .models import FileAudio
+from rest_framework import serializers
+
+
+class FileAudioSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = FileAudio
+        fields = ('id', 'upload', 'sha256')
