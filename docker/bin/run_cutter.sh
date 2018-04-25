b56a354cd8912d8466a0b3968aff5871a5af0531
Kacper Pikulski <pikulak1@gmail.com>
2018-04-25 05:00:32 +0200

    Rename cutter_api to files

--- docker/bin/run_cutter.sh
+++ docker/bin/run_cutter.sh
@@ -3,6 +3,6 @@
 /wait-for-it.sh postgres:5432 -t 0
 
 cd cutter
-python manage.py makemigrations cutter_api
+python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
