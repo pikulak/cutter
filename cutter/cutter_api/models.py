6e885c3a49091f1da009cc0d070644da36101887
Kacper Pikulski <pikulak1@gmail.com>
2018-04-25 03:15:22 +0200

    Fix: Django migrations doesn't support lambda

--- cutter/cutter_api/models.py
+++ cutter/cutter_api/models.py
@@ -4,12 +4,14 @@ from django.db import models
 from .utils import sha256_checksum
 
 
+def upload_file_path(instance, _):
+    return f'files/{instance.uuid}'
+
+
 class File(models.Model):
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
-    upload = models.FileField(
-        upload_to=lambda instance, _: f'files/{instance.uuid}'
-    )
+    upload = models.FileField(upload_to=upload_file_path)
     sha256 = models.CharField(max_length=60)
 
     def save(self, *args, **kwargs):
