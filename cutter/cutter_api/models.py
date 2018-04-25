1ab65f76f345cafc934b94bb210fa848a4bd9cc4
Kacper Pikulski <pikulak1@gmail.com>
2018-04-25 02:42:14 +0200

    Create File & FileAudio models

--- cutter/cutter_api/models.py
+++ cutter/cutter_api/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    upload = models.FileField(
+        upload_to=lambda instance, _: f'files/{instance.uuid}'
+    )
+    sha265 = models.CharField(max_length=60)
+
+
+class FileAudio(File):
+    pass
+
