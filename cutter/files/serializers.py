8caba5be526c601470bba4eb3af95e6feeb6532d
Kacper Pikulski <pikulak1@gmail.com>
2018-05-04 23:17:39 +0200

    Set processing_state to read only field in FileAudio serializer

--- cutter/files/serializers.py
+++ cutter/files/serializers.py
@@ -6,3 +6,4 @@ class FileAudioSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FileAudio
         fields = ('id', 'upload', 'processing_state')
+        read_only_fields = ('processing_state',)
