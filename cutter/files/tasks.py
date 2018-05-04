a7a1fe353330cb70319992f88502bf28c81d6cca
Kacper Pikulski <pikulak1@gmail.com>
2018-05-05 00:03:52 +0200

    Add provisional audio cutting

--- cutter/files/tasks.py
+++ cutter/files/tasks.py
@@ -1,10 +1,13 @@
-
 from uuid import UUID
 from celery import shared_task
 from .models import FileAudio
+from pydub import AudioSegment
 
 
 @shared_task
 def async_cut_file(file_id):
-    processed_file = FileAudio.objects.get(pk=UUID(file_id))
+    file = FileAudio.objects.get(pk=UUID(file_id))
+    song = AudioSegment.from_mp3(file.upload.path)
+    song_after_slice = song[:10*1000]
+    song_after_slice.export(file.upload.path)
     print("Processing file...")
